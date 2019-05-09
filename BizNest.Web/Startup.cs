3ff9a1de8d5d028d7d729f5b6e69a2f0a6fa23e2
Dayo Okesola <dayokesola@hotmail.com>
2019-05-09 22:17:30 +0100

    updated

--- BizNest.Web/Startup.cs
+++ BizNest.Web/Startup.cs
@@ -34,6 +34,7 @@ namespace BizNest.Web
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
