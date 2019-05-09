b1153c47278d6de5bbc2d8f7a0a498881db739b3
Dayo Okesola <dayokesola@hotmail.com>
2019-05-10 00:19:02 +0100

    Added Business and Stakeholder Entities

---
+++ BizNest.Core/Domain/Entity/App/Business.cs
@@ -0,0 +1,63 @@
+﻿namespace BizNest.Core.Domain.Entity.App
+{
+    /// <summary>
+    /// Business Class
+    /// </summary>
+    public class Business : BaseEntity<long>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Code { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int BusinessTypeId { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string AddressStreet { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string AddressCity { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string AddressPostCode { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int AddressCountryId { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Contact1Name { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Contact1Email { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Contact1Mobile { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Contact2Name { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Contact2Email { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Contact2Mobile { get; set; }
+
+    }
+
+}
---
+++ BizNest.Core/Domain/Entity/App/StakeHolder.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace BizNest.Core.Domain.Entity.App
+{
+    /// <summary>
+    /// StakeHolder Class
+    /// </summary>
+    public class StakeHolder : BaseEntity<long>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public long BusinessId { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string SSN { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Mobile { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime BirthDate { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string AddressStreet { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string AddressCity { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string AddressPostCode { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int AddressCountryId { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public decimal SeedCapital { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string BankName { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string AccountNumber { get; set; }
+
+    }
+
+}
