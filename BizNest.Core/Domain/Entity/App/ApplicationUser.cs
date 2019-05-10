af10527eabdb44b53faafec1270670fd07cf6490
Vincent Nwonah <vnwonah@outlook.com>
2019-05-10 01:08:58 +0100

    stash

---
+++ BizNest.Core/Domain/Entity/App/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BizNest.Core.Domain.Entity.App
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ BizNest.Service/Controllers/UserController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BizNest.Service.Controllers
+{
+    [Authorize(Policy = "ApiUser")]
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+
+        }
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var identity = await GetClaimsIdentity(model.Email, model.Password);
+
+                if (identity == null)
+                {
+                    return BadRequest(new ErrorResponse
+                    {
+                        ErrorDescription = "Your Email or Password is Incorrect"
+                    });
+                }
+                var jwt = await Tokens.GenerateJwt(identity, _jwtFactory, model.Email, _jwtOptions, new JsonSerializerSettings { Formatting = Formatting.Indented });
+
+                return Ok(new LoginResponse
+                {
+                    Token = jwt
+                });
+            }
+
+            return BadRequest(new ErrorResponse
+            {
+                ErrorDescription = "Your Email or Password is Incorrect"
+            });
+
+        }
+
+        private async Task<ClaimsIdentity> GetClaimsIdentity(string email, string password)
+        {
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+                return await Task.FromResult<ClaimsIdentity>(null);
+
+            // get the user to verifty
+            var userToVerify = await _userManager.FindByEmailAsync(email);
+
+            if (userToVerify == null) return await Task.FromResult<ClaimsIdentity>(null);
+
+            // check the credentials
+            if (await _userManager.CheckPasswordAsync(userToVerify, password))
+            {
+                return await Task.FromResult(_jwtFactory.GenerateClaimsIdentity(email, userToVerify.Id));
+            }
+
+            // Credentials are invalid, or account doesn't exist
+            return await Task.FromResult<ClaimsIdentity>(null);
+        }
+
+    }
+}
