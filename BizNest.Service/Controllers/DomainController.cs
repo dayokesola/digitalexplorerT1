e4d7e8f5f36e5f5ac19e3f9e414dbe8cb48f5ea4
Vincent Nwonah <vnwonah@outlook.com>
2019-05-11 08:35:29 +0100

    added domain search response model

--- BizNest.Service/Controllers/DomainController.cs
+++ BizNest.Service/Controllers/DomainController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BizNest.Service.Interfaces;
+using BizNest.Service.Models.RequestModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +13,25 @@ namespace BizNest.Service.Controllers
     [ApiController]
     public class DomainController : ControllerBase
     {
+        private IHttpService _httpService;
+        public DomainController(IHttpService httpService)
+        {
+            _httpService = httpService;
+        }
+        [HttpPost("domainsearch")]
+        public IActionResult DomainSearch(string domain)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(domain))
+                    return null;
+                var reqObj = new DomainSearchModel { Domain = domain.Replace(" ", "").Trim() + ".com" };
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
---
+++ BizNest.Service/Models/ResponseModels/DomainSearchResponse.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BizNest.Service.Models.ResponseModels
+{
+    public class DomainSearchResponse
+    {
+        [JsonProperty("domain")]
+        public string Domain { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("tld")]
+        public string TLD { get; set; }
+        [JsonProperty("tld_valid")]
+        public string TLD_Valid { get; set; }
+        [JsonProperty("available")]
+        public string Available { get; set; }
+    }
+}
