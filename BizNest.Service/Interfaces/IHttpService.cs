9067750159f92dce0170d6daf771056760101dde
Vincent Nwonah <vnwonah@outlook.com>
2019-05-11 08:22:21 +0100

    added generic http client

---
+++ BizNest.Service/Interfaces/IHttpService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BizNest.Service.Interfaces
+{
+    public interface IHttpService
+    {
+        (bool success, string message) SetClientHeader(string key, string value);
+        Task<TOut> GetAsync<TOut>(string url) where TOut : class;
+
+        Task<TOut> PostAsync<TOut, TIn>(TIn requestObject, string url) where TOut : class;
+    }
+}
